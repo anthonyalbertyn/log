627401e04e1d3287d713c03c93a4bee0452be6ea
Anthony Albertyn <anthonyalbertyn@gmail.com>
2020-10-11 19:41:11 +0100

    Work in progress

--- log/src/App.js
+++ log/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Button, Input, Modal, notification, Tabs } from 'antd';
 import { SearchOutlined, SoundOutlined, UserOutlined } from '@ant-design/icons';
 import ArtistForm from './components/ArtistForm';
